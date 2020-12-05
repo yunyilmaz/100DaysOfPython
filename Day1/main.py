4a83a3cdfe629c33d0f3e24deb10007118551a84
Yakup Unyilmaz <yakupunyilmaz@gmail.com>
2020-12-05 22:52:08 +0300

    Lenght of string

--- Day1/main.py
+++ Day1/main.py
@@ -1,5 +1,3 @@
-import sys
-print(sys.version)
 print("yakup")
 
 #Write your code below this line
@@ -7,4 +5,6 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
-print("Hello " + input("What is your name?"))
+name = input("What is your name?")
+print("Hello " + name)
+print("Length of name is:", len(name))
