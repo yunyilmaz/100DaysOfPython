c3966140895087e78ca7336ce1f423ff701bc83f
Yakup Unyilmaz <yakupunyilmaz@gmail.com>
2020-12-05 22:44:13 +0300

    Input example

--- Day1/main.py
+++ Day1/main.py
@@ -1,6 +1,10 @@
+import sys
+print(sys.version)
 print("yakup")
 
-#Write your code below this line 👇
+#Write your code below this line
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("Hello " + input("What is your name?"))
