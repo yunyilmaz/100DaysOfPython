0f6f2f525d02100ce350b7d45bd872a511b67e2b
Yakup Unyilmaz <yakupunyilmaz@gmail.com>
2020-12-05 23:09:15 +0300

    Band Name Generator

--- Day1/main.py
+++ Day1/main.py
@@ -1,10 +1,15 @@
-print("yakup")
+# print("yakup")
 
-#Write your code below this line
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# #Write your code below this line
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
-name = input("What is your name?")
-print("Hello " + name)
-print("Length of name is:", len(name))
+# name = input("What is your name?")
+# print("Hello " + name)
+# print("Length of name is:", len(name))
+
+print("Welcome to the Band Name Generator.")
+street = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
